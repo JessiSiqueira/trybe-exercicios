a33126970aeb3ecd5dc62c119fce556130728977
Jessica <jessica.siqueira.rj@gmail.com>
2023-03-28 18:18:05 -0300

    condicionais

--- fundamentos/secao-3-introducao-a-java-script/scripts.js
+++ fundamentos/secao-3-introducao-a-java-script/scripts.js
@@ -1,6 +1,12 @@
-let base = 5;
-let height = 8;
-let area = base * height;
-console.log(area)
-let perimeter = base * 2 + height *2;
-console.log(perimeter)
+let nota = 75;
+
+if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo de pessoas aprovadas");
+} else if (nota < 80 || nota === 60) {
+    console.log("Você está na nossa lista de espera");
+} else if (nota < 60) {
+    console.log("Infelizmente, você reprovou.");
+}
+
+console.log(nota)
+
