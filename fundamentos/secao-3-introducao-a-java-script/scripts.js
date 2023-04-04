de0b33973ccf103e7d9c5a4c809485f579dfdf22
Jessica <jessica.siqueira.rj@gmail.com>
2023-04-04 14:19:35 -0300

    Função

--- fundamentos/secao-3-introducao-a-java-script/scripts.js
+++ fundamentos/secao-3-introducao-a-java-script/scripts.js
@@ -1,1 +1,12 @@
-git 
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function cliente(nome) {
+    if(typeof nome === 'string') {
+        return clientesTrybeBank.push(nome);
+    } else {
+        return 'O parâmetro passado deve ser uma string'
+    }
+}
+
+console.log(cliente('Julio'));
+console.log(clientesTrybeBank);
